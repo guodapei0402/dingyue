16c425bf0e3a2483772f7aa9468b8571d07c1bd0
guodapei0402 <guodapei0402@163.com>
2025-11-13 19:15:07 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -13,7 +13,7 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-https://shz.al/~116-tg@pgkj666
+https://shz.al/~pgkg666
 `
 
 let urls = [];
@@ -831,6 +831,7 @@ async function KV(request, env, txt = 'ADD.txt', guest) {
 		});
 	}
 }
+
 
 
 
